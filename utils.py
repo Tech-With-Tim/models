a8ea93f0456d7b745a48213da2b9dc41a4e446c9
mohamed040406 <mohamed040406@users.noreply.github.com>
2021-07-07 14:35:08 +0200

    User and Token Models

---
+++ utils.py
@@ -0,0 +1,21 @@
+import datetime
+
+
+DISCORD_EPOCH = 1420070400000
+MAX_ASYNCIO_SECONDS = 3456000
+OUR_EPOCH = 1609459200
+
+
+def snowflake_time(id: int, *, internal: bool = True) -> datetime.datetime:
+    """
+    :param id:          The ID we want to convert.
+    :param internal:    Whether it's a internal ID or not.
+    :return:            :class:`datetime.datetime` instance.
+    """
+
+    epoch = OUR_EPOCH
+
+    if not internal:
+        epoch = DISCORD_EPOCH
+
+    return datetime.datetime.utcfromtimestamp(((id >> 22) + epoch) / 1000)
