50531e15ab5eac0db6c1d15469a396932f6f1b37
mohamed040406 <mohamed040406@users.noreply.github.com>
2021-07-07 15:10:44 +0200

    Whoops

--- __init__.py
+++ __init__.py
@@ -1,8 +1,8 @@
 from typing import Type, List
 from postDB import Model
 
-from users.user import User
-from users.token import Token
+from .users.user import User
+from .users.token import Token
 
 
 models_ordered: List[Type[Model]] = [User, Token]
