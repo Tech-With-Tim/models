ac55266db0e1ed37a1a63fbd786c4042c91fe67e
Shubhaankar <shubhaankar@hotmail.com>
2021-07-12 16:54:18 +0530

    add create_at and fetch

--- cdn/file.py
+++ cdn/file.py
@@ -1,5 +1,10 @@
 from postDB import Model, Column, types
 
+from typing import Union, Optional
+from datetime import datetime
+
+import utils
+
 
 class File(Model):
     """
@@ -18,3 +23,19 @@ class File(Model):
     mimetype = Column(types.String)
     name = Column(types.String)
     data = Column(types.Binary)
+
+    @classmethod
+    async def fetch(cls, id: Union[str, int]) -> Optional["File"]:
+        """Fetch a `File` with the given id."""
+        query = "SELECT * FROM files WHERE id = $1"
+        record = await cls.pool.fetchrow(query, int(id))
+
+        if record is None:
+            return None
+
+        return cls(**record)
+
+    @property
+    def created_at(self) -> "datetime":
+        """Returns the objects creation time in UTC."""
+        return utils.snowflake_time(id=self.id, internal=True)
