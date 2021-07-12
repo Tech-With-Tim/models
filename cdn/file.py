dbd291c2f5c5ce20c8445c59060425deafd09f44
Correct_skill <shubhaankar@hotmail.com>
2021-07-12 11:45:05 +0530

    Update file.py

--- cdn/file.py
+++ cdn/file.py
@@ -15,6 +15,6 @@ class File(Model):
     """
 
     id = Column(types.Integer(big=True), unique=True)
-    mimetype = Column(types.String)
     name = Column(types.String)
+    mimetype = Column(types.String)
     data = Column(types.Binary)
