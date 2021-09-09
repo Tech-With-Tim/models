b8b4189bfcac1a51536c67d7d99de2b0d2d4194e
Sylte <sylteandreas7@gmail.com>
2021-09-09 21:24:23 +0200

    Update Challenge model

--- challenges/challenge.py
+++ challenges/challenge.py
@@ -6,13 +6,19 @@ import utils
 
 class Challenge(Model):
     """
-    Challenge class to store the challenge details made by specific authors
+    Challenge class to store the challenge details
+
     Database Attributes:
-        Attributes stored in the `challenge` table.
-        :param int id:              The challenge ID
-        :param str title:           The challenge title.
-        :param int author_id:       The challenge author's Discord ID.
-        :param str description:     The challenge description.
+        Attributes stored in the `challenges` table.
+
+        :param int id:                          The challenge Snowflake ID.
+        :param str title:                       The challenge title.
+        :param int author_id:                   The challenge author's Discord ID.
+        :param str description:                 The challenge description.
+        :param int rules_id:                    The rules applied to this challenge.
+        :param int rewards_id:                  The rewards given to users who complete this challenge within time.
+        :param List[int] language_ids:          The languages you can use to complete this challenge.
+        :param :class:`datetime` released_at:   The time this challenge was released at.
     """
 
     id = Column(types.Integer(big=True), primary_key=True)
@@ -21,6 +27,14 @@ class Challenge(Model):
         types.ForeignKey("users", "id", sql_type=types.Integer(big=True)),
     )
     description = Column(types.String)
+    rules_id = Column(types.ForeignKey(
+        "rules", "id", sql_type=types.Integer(big=True))
+    )
+    rewards_id = Column(types.ForeignKey(
+        "challengerewards", "id", sql_type=types.Integer(big=True))
+    )
+    # Implicit ForeignKey to ChallengeLanguage.id
+    language_ids = Column(types.Array(types.Integer(big=True)))
     released_at = Column(types.DateTime, nullable=True)
 
     @property
