79df4149a2ed05a78f10f8ea35cd3ad1e4566274
Sylte <sylteandreas7@gmail.com>
2021-09-09 23:38:09 +0200

    Update ChallengeLanguage model

--- challenges/language.py
+++ challenges/language.py
@@ -10,19 +10,21 @@ class ChallengeLanguage(Model):
 
     Database Attributes:
         Attributes stored in the `challengeLanguages` table.
-        :param int id:                  The language ID
-        :param str name:                The language name
-        :param int download_url:
-        :param str piston_runtime:      The runtime used in piston to run the language
-        :param int disabled:
+
+        :param int id:                  Snowflake ID
+        :param str name:                Unique language name
+        :param int download_url:        Optional download url.
+        :param int disabled:            Whether or not this language is disabled.
+        :param str piston_lang:         The language this correlates to in piston API.
+        :param str piston_lang_ver:     The version of the specified language e.g. python (3.8.10)
     """
 
-    id = Column(types.Integer(big=True))
-    name = Column(types.String)
+    id = Column(types.Integer(big=True), primary_key=True)
+    name = Column(types.String, unique=True)
     download_url = Column(types.String)
-    piston_runtime = Column(types.String)
-    code_template = Column(types.String)
     disabled = Column(types.Boolean, default=False)
+    piston_lang = Column(types.String)
+    piston_lang_ver = Column(types.String)
 
     @property
     def created_at(self) -> datetime:
